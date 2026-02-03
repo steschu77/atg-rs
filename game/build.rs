570bdcd6825ed7eaaeb96bf9a38e8763023b7665
steschu77 <steschu77@gmail.com>
2026-02-03 20:33:41 +0100

    Update dependencies
    * Also ignore local Cargo.lock changes

--- game/build.rs
+++ game/build.rs
@@ -1,6 +1,6 @@
 extern crate embed_resource;
 
 fn main() {
-    embed_resource::compile("game.rc", embed_resource::NONE);
+    let _ = embed_resource::compile("game.rc", embed_resource::NONE);
     static_vcruntime::metabuild();
 }
